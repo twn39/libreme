5d1c7506ff9e48c5e05c96a1eb13b15b034218b1
twn39 <twn39@163.com>
2018-12-14 17:32:31 +0800

    update

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,7 @@
 export default {
   data() {
     return {
-      keyword: "php"
+      keyword: ""
     };
   },
 
@@ -14,11 +14,20 @@ export default {
   render() {
     return (
       <div class="home">
-        <input type="text" onKeyup={this.change} />
+          <div>
+              <input type="text" onKeyup={this.change} />
 
-        <a href={"https://github.com/search?q=" + this.keyword} target="_blank">
-          github
-        </a>
+          </div>
+
+          <div>
+              <a href={"https://github.com/search?q=" + this.keyword} target="_blank">Github</a>
+              <a href={"https://developer.mozilla.org/zh-CN/search?q=" + this.keyword} target="_blank">MDN</a>
+              <a href={"https://stackoverflow.com/search?q="+this.keyword} target="_blank">Stack Overflow</a>
+              <a href={"https://www.google.com/search?q="+ this.keyword} target="_blank">Google</a>
+              <a href={"https://cn.bing.com/search?q="+ this.keyword} target="_blank">Bing</a>
+              <a href={"https://duckduckgo.com/?q="+this.keyword} target="_blank">DuckDuckGo</a>
+              <a href={"https://zh.wikipedia.org/wiki/"+this.keyword}>Wikipedia</a>
+          </div>
       </div>
     );
   }
